c720bb54fca528cd24341599a8b3faef82bb1e1a
SamOr1014 <orchamsumsam1014@gmail.com>
2024-04-06 14:12:03 +0100

    chore: setup vite test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3030,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["src/setupTest.ts"],
+  },
+});
