1b0309789c602606053c57e10146abd005416ce7
SamOr1014 <orchamsumsam1014@gmail.com>
2024-04-06 14:41:17 +0100

    feat: setup shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: ['src/setupTest.ts'],
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 });
