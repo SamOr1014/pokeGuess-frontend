45094c4d509b416be356716f1486172f370b1ba0
SamOr1014 <orchamsumsam1014@gmail.com>
2024-04-06 14:27:15 +0100

    chore: set up eslint and prettier

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,17 +1,17 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 // To Test
-import App from "../App";
+import App from '../App';
 
 // Tests
-describe("Renders main page correctly", async () => {
-  it("Should render the page correctly", async () => {
-    // Setup
-    render(<App />);
-    const h1 = await screen.queryByText("app");
+describe('Renders main page correctly', async () => {
+    it('Should render the page correctly', async () => {
+        // Setup
+        render(<App />);
+        const h1 = await screen.queryByText('app');
 
-    // Expectations
-    expect(h1).toBeInTheDocument();
-  });
+        // Expectations
+        expect(h1).toBeInTheDocument();
+    });
 });
